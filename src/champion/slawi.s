e340f86d10f94916612361be0f554679eea3dc20
nayarix <ilyassukun@gmail.com>
2020-01-24 05:08:53 +0100

    update slawi player

--- src/champion/slawi.s
+++ src/champion/slawi.s
@@ -3,13 +3,10 @@
 
 start:	sti r1, %:live1, %1
 		ld %0, r5
-		zjmp %:label1
 
 live1:	live %1
-
-label1: sti r1, %:live2, %1
-		fork %15
-		add r8, r8, r8
+		sti r1, %:live2, %1
+		fork %:live2
 		zjmp %:live1
 
 live2: live %2
